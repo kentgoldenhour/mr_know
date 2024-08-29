1da3e0d7bb712cf37e3b3945f0c0cc27cd2d7ecf
Kent Alber Fredson <kent.fredson@binus.edu>
2024-08-30 05:11:10 +0700

    fix: change api key

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -10,7 +10,7 @@ import {
     HarmBlockThreshold,
   } from "@google/generative-ai";
   
-  const apiKey = "AIzaSyCKNc2e2MuDefpuZGv7WuntY5GMG78Qn_o";
+  const apiKey = "YOUR_API_KEY";
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
